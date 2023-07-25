924113e28223e4aa48004dad7671fe22a4a057e5
Jimmy Li <jmyli562@gmail.com>
2023-07-25 13:26:29 -0700

    finished lab

---
+++ lib/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',2,'M','Brown','Evil',TRUE),
+('Rowdy',2,'M','Brown','Evil',TRUE),('Tabitha',2,'M','Brown','Evil',TRUE),('Sergeant Brown',2,'M','Brown','Evil',TRUE),
+('Melissa',2,'M','Brown','Evil',TRUE),('Grinch',2,'M','Brown','Evil',TRUE),('Wendy',2,'M','Brown','Evil',TRUE),(NULL,2,'M','Brown','Evil',TRUE)
